d428e16ce6786495e236e977f3f28dd537bb5c5e
Joel Ross <joelross@uw.edu>
2016-01-14 12:27:06 -0800

    Add end of lecture code (inc. errors)

--- lecture/app/src/main/java/edu/uw/listdatademo/InputControlActivity.java
+++ lecture/app/src/main/java/edu/uw/listdatademo/InputControlActivity.java
@@ -2,12 +2,36 @@ package edu.uw.listdatademo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class InputControlActivity extends AppCompatActivity {
+
+    private static final String TAG = "InputControl";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.input_control_layout);
+
+        final EditText text = (EditText)findViewById(R.id.txtSearch);
+        final Button button = (Button)findViewById(R.id.btnSearch);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            private int count = 1;
+
+            @Override
+            public void onClick(View v) {
+                Log.v(TAG, "Button was pressed "+count+" times!");
+                count++;
+                String typed = text.getText().toString();
+                Log.v(TAG, "You typed: "+typed);
+            }
+        });
+
+
+
     }
 }
--- lecture/app/src/main/java/edu/uw/listdatademo/ListActivity.java
+++ lecture/app/src/main/java/edu/uw/listdatademo/ListActivity.java
@@ -1,13 +1,137 @@
 package edu.uw.listdatademo;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.ListView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.Arrays;
 
 public class ListActivity extends AppCompatActivity {
+
+
+    private ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
+        //model!
+        String[] data = new String[99];
+        for(int i=99; i>0; i--){
+            data[99-i] = i+ " bottles of beer on the wall";
+        }
+
+        //String[] data = downloadMovieData("Die Hard");
+
+
+        MovieDownloadTask task = new MovieDownloadTask();
+        task.execute("Die Hard");
+
+
+        //controller
+        adapter = new ArrayAdapter<String>(
+                this, R.layout.list_item, R.id.txtItem, data);
+
+        //support ListView or GridView
+        AdapterView listView = (AdapterView)findViewById(R.id.listView);
+        listView.setAdapter(adapter);
+
+
+
+    }
+
+    public class MovieDownloadTask extends AsyncTask<String, Void, String[]> {
+
+        protected String[] doInBackground(String... params){
+
+            String movie = params[0];
+
+            //construct the url for the omdbapi API
+            String urlString = "";
+            try {
+                urlString = "http://www.omdbapi.com/?s=" + URLEncoder.encode(movie, "UTF-8") + "&type=movie";
+            }catch(UnsupportedEncodingException uee){
+                return null;
+            }
+
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            String movies[] = null;
+
+            try {
+
+                URL url = new URL(urlString);
+
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    return null;
+                }
+                String results = buffer.toString();
+                results = results.replace("{\"Search\":[","");
+                results = results.replace("]}","");
+                results = results.replace("},", "},\n");
+                Log.v("ListActivity", results);
+                movies = results.split("\n");
+
+            }
+            catch (IOException e) {
+                return null;
+            }
+            finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    }
+                    catch (final IOException e) {
+                    }
+                }
+            }
+
+            return movies;
+        }
+
+
+        protected void onPostExecute(String[] movies){
+
+            adapter.clear();
+            for(String movie : movies){
+                adapter.add(movie);
+            }
+
+        }
+
     }
 }
