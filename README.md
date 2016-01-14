6d35106387a5d13e3615d638815e2c0a890ae390
Joel Ross <joelross@uw.edu>
2016-01-14 08:46:11 -0800

    Add lecture starter code/demo

---
+++ README.md
@@ -0,0 +1,7 @@
+# 01-14 Lecture and Lab code
+Code demoing input controls, list views, and network data access
+
+
+### Lab code is in the `lab` folder
+Instructions in that folder's README.
+
---
+++ lecture/app/src/main/java/edu/uw/listdatademo/InputControlActivity.java
@@ -0,0 +1,13 @@
+package edu.uw.listdatademo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InputControlActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.input_control_layout);
+    }
+}
